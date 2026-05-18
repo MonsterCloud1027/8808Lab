352a8bcd2538a37e260b951956a8d73ff8209ec0
Usmanfye <152373039+Usmanfye@users.noreply.github.com>
2026-05-17 21:26:32 -0400

    Add files via upload

---
+++ legend.js
@@ -0,0 +1,31 @@
+/**
+ * Draws a legend in the area at the bottom of the screen, corresponding to the bars' colors
+ *
+ * @param {string[]} data The data to be used to draw the legend elements
+ * @param {*} color The color scale used throughout the visualisation
+ */
+export function draw (data, color) {
+  // TODO : Generate the legend in the div with class "legend". Each SVG rectangle
+  // should have a width and height set to 15.
+  // Tip : Append one div per legend element using class "legend-element".
+  const legend = d3.select('.legend')
+
+  const items = legend
+    .selectAll('.legend-element')
+    .data(data)
+    .join('div')
+    .attr('class', 'legend-element')
+
+  items
+    .append('svg')
+    .attr('width', 15)
+    .attr('height', 15)
+    .append('rect')
+    .attr('width', 15)
+    .attr('height', 15)
+    .attr('fill', d => color(d))
+
+  items
+    .append('span')
+    .text(d => d)
+  }
