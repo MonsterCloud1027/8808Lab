9e6b65110b40032d1d9f56ae5154e877da7b310a
Usmanfye <ofaye7492@gmail.com>
2026-05-25 20:14:46 -0400

    Initial commit for sharing

---
+++ tooltip.js
@@ -0,0 +1,28 @@
+/**
+ * Defines the contents of the tooltip.
+ *
+ * @param {object} d The data associated to the hovered element
+ * @returns {string} The tooltip contents
+ */
+
+export function getContents (d) {
+  /* TODO : Define and return the tooltip contents including :
+      + A title stating the hovered element's group, with:
+        - Font family: Grenze Gotish
+        - Font size: 24px
+        - Font weigth: normal
+      + A bold label for the player name followed
+        by the hovered elements's player's name
+      + A bold label for the player's line count
+        followed by the number of lines
+  */
+  return `
+    <div>
+      <div style="font-family: 'Grenze Gotisch'; font-size: 24px; font-weight: normal;">
+        Act ${d.Act}
+      </div>
+      <div><b>Player : </b>${d.Player}</div>
+      <div><b>Lines : </b>${d.Count}</div>
+    </div>
+  `
+}
